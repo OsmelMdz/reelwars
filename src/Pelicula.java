6d0e3519716d19fb04a5ae62163d406183a5ae38
OsmelMdz <mdz.hectorosmel@gmail.com>
2025-05-03 01:24:43 -0600

    Initial commit

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -1,7 +1,8 @@
-public record Pelicula(String title,
-                       int episode_id,
-                       String opening_crawl,
-                       String director,
-                       String producer,
-                       String release_date) {
-}
+public record Pelicula(
+        String title,
+        int episode_id,
+        String opening_crawl,
+        String director,
+        String producer,
+        String release_date
+) {}
